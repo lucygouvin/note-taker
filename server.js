ac5d9178b7345f4f863c758bd8a5c00383249fb9
lgouvin <gouvin.lucy@gmail.com>
2023-08-16 18:13:59 -0400

    fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const api = require('./routes/index')
 
 // Set the port and instantiate express app
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 const app = express();
 
 // Middleware
