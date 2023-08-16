ceec451798a58104c01d9738a5619410b065acd0
lgouvin <gouvin.lucy@gmail.com>
2023-08-16 18:06:17 -0400

    comment

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', api)
+// comment
 
 // Serve index.html
 app.get('/', (req, res) =>
